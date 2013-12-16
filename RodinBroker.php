32d1269cd2d7faec9b431f84f37d1995e6f387d4
Javier Belmonte <javier.belmonte@hesge.ch>
2013-12-16 15:45:09 +0100

    Gives the client the possibility to receive a proxy configuration to use during a not-to-rodin-server call

--- RodinBroker.php
+++ RodinBroker.php
@@ -15,20 +15,26 @@ class RodinBroker {
 	const METHOD_PUT = 2;
 	const METHOD_DELETE = 3;
 
-	public static function makeCallToServer($method = RodinBroker::METHOD_GET, $resource, $parameters = NULL) {
-		if ($parameters == NULL)
+	public static function makeCallToServer($method = RodinBroker::METHOD_GET, $resource, $parameters = null) {
+		if ($parameters == null)
 			return RodinBroker::makeCall($method, RodinBroker::RODIN_SERVER . $resource);
 		else
 			return RodinBroker::makeCall($method, RodinBroker::RODIN_SERVER . $resource, $parameters);
 	}
 
-	public static function makeCall($method = RodinBroker::METHOD_GET, $url, $parameters = NULL) {
+	public static function makeCall($method = RodinBroker::METHOD_GET, $url, $parameters = null, $proxy = null) {
 		switch ($method) {
 			case RodinBroker::METHOD_GET:
-				$request = \Httpful\Request::get($url)->mime('application/json');
-				$response = $request->send();
-
-				return $response;
+				if ($proxy == null) {
+					$request = \Httpful\Request::get($url, 'application/json');
+					$response = $request->send();
+					return $response;
+				} else {
+					$request = \Httpful\Request::get($url, 'application/json')->useProxy($proxy['host'], intval($proxy['port']),
+						CURLAUTH_BASIC, $proxy['user'], $proxy['password']);
+					$response = $request->send();
+					return $response;
+				}
 			case RodinBroker::METHOD_POST:
 			case RodinBroker::METHOD_PUT:
 			case RodinBroker::METHOD_DELETE:
